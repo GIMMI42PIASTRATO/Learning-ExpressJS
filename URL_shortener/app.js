56c27df42fd3aa4325e70194d21eb5836a16d9a7
GIMMI42PIASTRATO <67071988+GIMMI42PIASTRATO@users.noreply.github.com>
2024-02-07 17:55:07 +0100

    Add Bootstrap styling and started the shrink functionality

--- URL_shortener/app.js
+++ URL_shortener/app.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const path = require("path");
+const { nanoid } = require("nanoid");
+
 const app = express();
 
 app.listen(process.env.port || 3000, () => {
 	console.log("App is listening on http://localhost:3000");
 });
-
+app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
-
 // By default app.set("view engine", "ejs"); it goes and looks for the views folder inside the root, here we specify instead to look for it inside the folder we are in
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static("views"));
@@ -16,4 +17,9 @@ app.get("/", (req, res) => {
 	res.render("index");
 });
 
-console.log(__dirname);
+app.post("/shrink", (req, res) => {
+	const longURL = req.body.url;
+	const uniqueID = nanoid(7);
+	console.log(uniqueID);
+	res.redirect("/");
+});
